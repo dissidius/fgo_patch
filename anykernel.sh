35796a7f717d3b401c6167bd4a63ce09168f4435
froz3n <son.froz.arc@gmail.com>
2017-07-05 01:04:24 +0700

    add detect boot function
    there's probably some that is missing still

--- anykernel.sh
+++ anykernel.sh
@@ -17,13 +17,33 @@ device.name5=
 } # end properties
 
 # shell variables
-block=/dev/block/bootdevice/by-name/boot;
+#block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . /tmp/anykernel/tools/ak2-core.sh;
+
+#detect boot 
+find_boot_image() {
+  if [ -z "$block" ]; then
+    for PARTITION in kern-a KERN-A android_boot ANDROID_BOOT kernel KERNEL boot BOOT lnx LNX; do
+      block=`readlink /dev/block/by-name/$PARTITION || readlink /dev/block/platform/*/by-name/$PARTITION || readlink /dev/block/platform/*/*/by-name/$PARTITION`
+      if [ ! -z "$block" ]; then break; fi
+    done
+  fi
+  if [ -z "$block" ]; then
+    FSTAB="/etc/recovery.fstab"
+    [ ! -f "$FSTAB" ] && FSTAB="/etc/recovery.fstab.bak"
+    [ -f "$FSTAB" ] && block=`grep -E '\b/boot\b' "$FSTAB" | grep -oE '/dev/[a-zA-Z0-9_./-]*'`
+  fi
+  if [ -z "$block" ]; then
+    ui_print " "; 
+	ui_print "Unable to determine active boot slot. Aborting..."; 
+	exit 1;
+  fi;
+}
 
 
 ## AnyKernel permissions
@@ -32,8 +52,11 @@ is_slot_device=0;
 #chmod 644 $ramdisk/sbin/media_profiles.xml
 
 
-ui_print "[#] Updating default.prop..."
 ## AnyKernel install
+find_boot_image;
+
+ui_print "[#] Updating default.prop..."
+
 dump_boot;
 
 # begin ramdisk changes
